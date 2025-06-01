28b71e7b66533da97a34d5e4493ae02b2b9ced3a
Shadan Asad <shadanasad@Shadans-MacBook-Air.local>
2025-06-01 12:42:42 +0530

    Updated to type definitions and typescript configs

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
